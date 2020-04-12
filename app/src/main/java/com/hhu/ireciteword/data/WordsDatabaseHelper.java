21e386ff8fc6f4aac863207a49f9a4dd1638fa66
JiRui <RyneJi@Gmail.com>
2020-04-12 12:40:27 +0800

    ...

--- app/src/main/java/com/hhu/ireciteword/data/WordsDatabaseHelper.java
+++ app/src/main/java/com/hhu/ireciteword/data/WordsDatabaseHelper.java
@@ -1,4 +1,26 @@
 package com.hhu.ireciteword.data;
 
-public class WordsDatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WordsDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME="words.db";
+
+    private static final int DATABASE_VERSION=1;
+
+    public WordsDatabaseHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
