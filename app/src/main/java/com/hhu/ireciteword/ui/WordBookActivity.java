da661c62e5588c5a3e9d4d5d8929035a77b651dd
M&S <1245504869@qq.c>
2020-05-17 20:49:04 +0800

    添加了初步单词书页面的功能

--- app/src/main/java/com/hhu/ireciteword/ui/WordBookActivity.java
+++ app/src/main/java/com/hhu/ireciteword/ui/WordBookActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hhu.ireciteword.R;
 
 public class WordBookActivity extends AppCompatActivity {
 
-    private TextView tv;
     private TextView tvTitle;
     private ImageView ivBack;
     private String content;
+    private RadioGroup mRg1;
+    private RadioButton mRb_1, mRb_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +26,25 @@ public class WordBookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word_book);
 
         content=getIntent().getStringExtra("content");
-        tv=findViewById(R.id.tv);
         tvTitle=findViewById(R.id.tv_title);
         ivBack=findViewById(R.id.iv_back);
         tvTitle.setText(content);
-        tv.setText("这是"+content+"界面");
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        mRg1=findViewById(R.id.rg_1);
+        mRb_1=findViewById(R.id.rb_1);
+        mRb_2=findViewById(R.id.rb_2);
+
+        mRg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {   //监听事件
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                Toast.makeText(WordBookActivity.this,radioButton.getText(),Toast.LENGTH_SHORT).show();
             }
         });
     }
